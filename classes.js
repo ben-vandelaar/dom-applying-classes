2782d3523c883ad8026bc6bc6ad82734f3079a15
ben-vandelaar <benvandelaar@gmail.com>
2018-06-25 15:58:09 +1200

    made Visible

--- classes.js
+++ classes.js
@@ -7,7 +7,8 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
  one()
  two()
- three()   
+ three()
+ makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -31,3 +32,7 @@ function three (){
   three.classList.add('cyan')
   }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible(){
+  var li = document.getElementsByClassName("invisible");
+  li[0].className = "visible";
+}
